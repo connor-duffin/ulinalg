14cdeef55282011274c41757b950bd62a6e620f7
Connor Duffin <connor.p.duffin@gmail.com>
2025-03-20 12:11:35 +0000

    Basic CMakeLists setup

--- src/array.hpp
+++ src/array.hpp
@@ -14,11 +14,14 @@ public:
   int get_nrow();
   int get_ncol();
   std::vector<int> get_vals();
+
   void set_zeros();
   void set_ones();
   void set_vals(std::vector<int>&);
+
   Array bcast(Array&);
-  Array operator+(Array const&);
+  Array operator+(Array const &);
+
   void pprint();
 };
 
